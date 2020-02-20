81410740522bd6ccc063e9dd8e2ebe76fc2bfee8
Petr Manek <petrmanek@users.noreply.github.com>
2020-02-20 19:46:26 +0000

    add PM among package authors

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ATE',
     version='1.0.0',
-    author='Graham Van Goffrier',
+    author='Graham Van Goffrier, Petr Mánek',
     packages=find_packages(exclude=['*test']),
     install_requires=['numpy', 'matplotlib', 'pandas', 'docker'],
     # entry_points={
