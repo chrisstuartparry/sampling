dcdb52f02226455938ff9325aaa5c6c2cffc7e06
Petr Manek <petrmanek@users.noreply.github.com>
2020-02-20 19:22:19 +0000

    update unit test

--- ATE/tests/test_classes.py
+++ ATE/tests/test_classes.py
@@ -2,22 +2,22 @@
 Simple tests and data generation.
 """
 
-from ATE import Samplerun, Parameter
+from .. import UniformSamplingStrategy, Domain, Samplerun
 import os
 
+
 def test_samplerun():
-	"""
-	Sets up uniform geemeration of data points and csv output.
-	"""
+    """
+    Sets up uniform geemeration of data points and csv output.
+    """
 
-	numsamples = 100
-	
-	run1 = Samplerun(numsamples)
-	run1.setup_model()
-	run1.perform_sample(savefile="100uniform.csv")
-		
-	
+    numsamples = 100
+    domain = Domain()
+    sampling_strategy = UniformSamplingStrategy()
 
-if __name__== "__main__":
-	test_samplerun()
+    run1 = Samplerun(numsamples, domain, sampling_strategy)
+    run1.perform_sample(savefile="100uniform.csv")
 
+
+if __name__ == "__main__":
+    test_samplerun()
