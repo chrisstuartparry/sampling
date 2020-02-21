4980adddb0be79d4961226302ad58272f8842639
Petr Manek <petrmanek@users.noreply.github.com>
2020-02-21 12:03:54 +0000

    reduce sample count in the sampling test, fix parameter name

--- ATE/tests/test_classes.py
+++ ATE/tests/test_classes.py
@@ -11,12 +11,12 @@ def test_samplerun():
     Sets up uniform geemeration of data points and csv output.
     '''
 
-    numsamples = 100
+    numsamples = 5
     domain = Domain()
     sampling_strategy = UniformSamplingStrategy()
 
     run1 = Samplerun(numsamples, domain, sampling_strategy)
-    run1.perform_sample(savefile='100uniform.csv')
+    run1.perform_sample(out_file='100uniform.csv')
 
 
 if __name__ == '__main__':
