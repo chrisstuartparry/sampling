ea7e31c03ee12576ad0423998cb6c44a288f6d56
Petr Manek <petrmanek@users.noreply.github.com>
2020-02-21 12:03:26 +0000

    add sklearn dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version='1.0.0',
     author='Graham Van Goffrier, Petr Mánek',
     packages=find_packages(exclude=['*tests']),
-    install_requires=['numpy', 'matplotlib', 'pandas', 'docker'],
+    install_requires=['numpy', 'matplotlib',
+                      'pandas', 'docker', 'scikit-learn'],
     # entry_points={
     #    'console_scripts': [
     #        'tbrsample = ATE.command:process'
