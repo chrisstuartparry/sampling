223869be74504027dd73bc77c6901c0386479842
Petr Manek <petrmanek@users.noreply.github.com>
2020-02-21 12:03:20 +0000

    add sklearn utilities

--- ATE/__init__.py
+++ ATE/__init__.py
@@ -4,3 +4,6 @@ from .run import Samplerun
 from .sampling import UniformSamplingStrategy
 from .data_utils import encode_data_frame
 from .data_utils import x_y_split
+from .train import apply_on_y_columns
+from .train import fit_multiple
+from .train import predict_multiple
