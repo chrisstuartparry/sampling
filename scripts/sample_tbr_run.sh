de99b9c152962de7f1abd8cb2795ef7a33f7f4d8
Petr Manek <petrmanek@users.noreply.github.com>
2020-02-22 22:43:29 +0000

    fix siamese bug

--- scripts/sample_tbr_run.sh
+++ scripts/sample_tbr_run.sh
@@ -7,14 +7,17 @@ export TIME_SPIN_UP=5 # seconds
 export RUN_NO=0
 export BATCH_NO=$1 ; shift # TODO: make clear that this is a parameter
 export RUN_DIR=${MAIN_DIR}/data/run${RUN_NO}
+export IMAGE_NAME=tbr26.sif
 
 set -e
 
-cd ${MAIN_DIR}/run
+cp ${MAIN_DIR}/images/${IMAGE_NAME} /tmp/${IMAGE_NAME}
+
+cd /tmp
 source ${MAIN_DIR}/sampling/venv/bin/activate
 
 echo "Running simulation server"
-singularity exec ${MAIN_DIR}/images/tbr26.sif python3 /app/simple_sphere_TBR_study/flask_api_for_tbr_simulation.py &
+singularity exec /tmp/${IMAGE_NAME} python3 /app/simple_sphere_TBR_study/flask_api_for_tbr_simulation.py &
 
 echo "Sleeping ${TIME_SPIN_UP} seconds"
 sleep ${TIME_SPIN_UP}
@@ -27,4 +30,6 @@ kill %1
 wait < <(jobs -p)
 
 deactivate
+
+rm -f /tmp/${IMAGE_NAME}
 echo "Done"
