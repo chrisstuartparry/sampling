f6edc02f17ac8c1cc02654b789780127b50bc135
Petr Manek <petrmanek@users.noreply.github.com>
2020-02-22 18:29:28 +0000

    add param generation test

---
+++ ATE/tests/test_generate_params.py
@@ -0,0 +1,19 @@
+from .. import UniformSamplingStrategy, Domain
+import numpy as np
+import os
+
+
+def test_generate_params():
+    '''
+    Sets up uniform geemeration of data points and csv output.
+    '''
+
+    n_samples = 1000
+    seed = 1
+
+    np.random.seed(seed)
+
+    domain = Domain()
+    sampling_strategy = UniformSamplingStrategy()
+    df = domain.gen_data_frame(sampling_strategy, n_samples)
+    df.to_csv('output/1000params.csv', index=False)
