6f2095e903755d65cec3a70a9108ca47c9408548
Graham Van Goffrier <vangoffrier@gmail.com>
2020-02-26 15:41:15 +0000

    Calculate variograms for all discretised run0 data.

--- ATE/tests/test_variogram.py
+++ ATE/tests/test_variogram.py
@@ -5,19 +5,21 @@ given discrete-sliced sample set.
 
 from ATE import UniformSamplingStrategy, Domain, Samplerun, data_utils
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import pprint
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
+from pprint import pprint
+from skgstat import Variogram
 
 
-def test_pca():
+def test_variogram():
     '''
     Performs PCA on all discrete-slices from run.
     '''
-    pcasumvar = {}
+    
+    var_descs = {}
     
     indir = "output/run0dsort/"
     for filename in os.listdir(indir):
@@ -25,6 +27,8 @@ def test_pca():
     
             # load dataset into Pandas DataFrame
             fpath = os.path.join(indir,filename)
+            runname = os.path.splitext(filename)[0]
+            
             df = pd.read_csv(fpath, index_col=0)
     
             # separate out cont and disc features, and target
@@ -33,39 +37,66 @@ def test_pca():
             # construct pca space consisting of continuous features and target
             cy = pd.concat([c, y[['tbr']]], axis = 1)
 
-            numcomp = cy.columns.values.size
-            cy = StandardScaler().fit_transform(cy)
- 
-            pca = PCA(n_components=numcomp)
-            principalComponents = pca.fit_transform(cy)
-            principalDf = pd.DataFrame(data = principalComponents, 
-			           columns = ['pc1', 'pc2', 'pc3', 'pc4',
-				              'pc5', 'pc6', 'pc7', 'pc8',
-					      'pc9', 'pc10','pc11','pc12',
-					      'pc13'])
-
-            #fig = plt.figure(figsize = (8,8))
-            #ax = fig.add_subplot(1,1,1) 
-            #ax.set_xlabel('Principal Component 1', fontsize = 15)
-            #ax.set_ylabel('Principal Component 2', fontsize = 15)
-            #ax.set_title('2 component PCA', fontsize = 20)
-            #ax.scatter(principalDf['pc1'],
-             #           principalDf['pc2'],
-            #           s = 50)
-            #ax.grid()	                    
-            #fig.savefig('pca_plot.jpg')
-    
-            pcavar = np.array(pca.explained_variance_ratio_)
-            this_pcasumvar = np.round(pcavar.cumsum()*100,decimals=1)
-            pcasumvar[os.path.splitext(filename)[0]] = this_pcasumvar
-    
-            with open("pca_out/result" + filename, "ab") as f:
-                np.savetxt(f, this_pcasumvar, delimiter=',')
-                f.write(b"\n")
-                np.savetxt(f, pca.components_, delimiter=',')
+            numcomp = y[['tbr']].values.size
+            yvalues = [x[0] for x in y[['tbr']].values]
             
-    with open("pca_out/pcasumvar.csv", 'w') as f:
-        pprint.pprint(pcasumvar, f)
+            # Calculate variogram of TBR over all parameter values
+            V = Variogram(coordinates=c.values, values=yvalues,
+                          model='matern', normalize=False,
+                          maxlag=200, n_lags=100)
+                          
+            #fig = V.plot()              
+                    
+            #fig.savefig('var_out/var_plot' + runname + '.jpg')
+            #fig.show()
+            
+            var_descs[runname] = V.describe()
+            
+            
+    with open("var_out/vardescs.csv", 'w') as f:
+        pprint(var_descs, f)
+        
+def test_variogram_all():
+    '''
+    Performs PCA on all discrete-slices from run.
+    '''
+    
+    
+    indir = "output/run0dsort/"
+    for filename in os.listdir(indir):
+        if filename.endswith(".csv"):
+    
+            # load dataset into Pandas DataFrame
+            fpath = os.path.join(indir,filename)
+            runname = os.path.splitext(filename)[0]
+            
+            if 'df' in locals():
+                df = df.append(pd.read_csv(fpath, index_col=0), ignore_index=True)
+            else:
+                df = pd.read_csv(fpath, index_col=0)
+    
+    # separate out cont and disc features, and target
+    c, d, y = data_utils.c_d_y_split(df)
+
+    # construct pca space consisting of continuous features and target
+    cy = pd.concat([c, y[['tbr']]], axis = 1)
+    numcomp = y[['tbr']].values.size
+    yvalues = [x[0] for x in y[['tbr']].values]
+            
+    # Calculate variogram of TBR over all parameter values
+    V = Variogram(coordinates=c.values, values=yvalues,
+                  model='matern', normalize=False,
+                  maxlag=200, n_lags=100)
+                  
+    fig = V.plot()  
+    pause            
+            
+    fig.savefig('var_out/var_plot_all.jpg')
+    #fig.show()
+            
+    with open("var_out/vardescs_all.csv", 'w') as f:
+        pprint(V.describe(), f)
 
 if __name__ == '__main__':
-    test_pca()
+    os.environ['SKG_SUPPRESS'] = "true"
+    test_variogram_all()
