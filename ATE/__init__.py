a8bb628a9feb1e80fb3033946189f4ffb43d11c6
Petr Manek <petrmanek@users.noreply.github.com>
2020-03-02 08:33:01 +0000

    export SumParameterGroup

--- ATE/__init__.py
+++ ATE/__init__.py
@@ -1,6 +1,7 @@
 from .param import Parameter
 from .param import DiscreteParameter
 from .param import ContinuousParameter
+from .param import SumParameterGroup
 from .domain import Domain
 from .run import Samplerun
 from .sampling import UniformSamplingStrategy
